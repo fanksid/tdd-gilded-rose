4ce4ecf2dbef644ca7b7032c6add16ede3159273
Kong Fanyue <fykong@thoughtworks.com>
2020-03-23 23:13:49 +0800

    feat: add quality should less than 50 task

--- src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
@@ -6,11 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ProductTest {
 
     @Test
-    void should_return_quality_no_less_than_0() {
+    void should_return_quality_no_less_than_0_given_many_days_passed() {
         Product product = new Product(50, 20, 2);
 
         int quality = product.getCurrentQuality(30);
 
         assertTrue(quality >= 0);
+    }
+
+    @Test
+    void should_return_quality_no_more_than_50_given_few_days_passed() {
+        Product product = new Product(50, 20, 2);
+
+        int quality = product.getCurrentQuality(0);
+
+        assertTrue(quality <= 50);
     }
 }
